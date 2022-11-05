766d6e3158fb7074a18fb2fac32b264d5753615e
Nayem-Mursalin <nayem.mursalin19@gmail.com>
2022-11-05 14:36:33 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5500;
+
+//middle ware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.c8rri4v.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+app.get('/', (req, res) => {
+    res.send('Ema John Server is Running');
+})
+app.listen(port, () => {
+    console.log(`ema john runing on port: ${port}`);
+})
